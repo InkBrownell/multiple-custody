65773bbad252a0affede3b07c53eb9cbf22f2818
David Brownell <davidabrownell@gmail.com>
2020-12-15 22:14:43 -0800

    Add multi-level encrypt/decrypt functionality

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 """Contains EncryptedShare"""
-from typing import List
+import itertools
+import re
+from functools import partial
+from typing import List, Dict
 
 from Crypto.Hash import SHA3_256
 from Crypto.Protocol.SecretSharing import Shamir
+from sympy import Symbol, simplify_logic, true, to_cnf
+
+from sympy.logic import And, Or
 
 
 class EncryptedShare:
     """An object representing a single SSSS share."""
-    def __init__(self, name, ciphertext, schema, hashed_plaintext):
+
+    def __init__(self, name, ciphertexts, schema, hashed_plaintext):
         self.name = name
-        self.ciphertext = ciphertext
+        self.ciphertexts = ciphertexts
         self.schema = schema
         self.hashed_plaintext = hashed_plaintext
 
@@ -21,29 +28,58 @@ class EncryptedShare:
         :param schema: The way the data should be encoded
         :returns: A list of encrypted shares
         :rtype: List[EncryptedShare]"""
-        if len(schema) != 1:
-            raise ValueError('Schema must have exactly one top level expression')
-
-        if (lst := schema.get('any')) or (lst := schema.get('or')):
-            threshold = 1
-            num_shares = len(lst)
-        elif (lst := schema.get('all')) or (lst := schema.get('and')):
-            threshold = num_shares = len(lst)
-        else:
-            raise ValueError(f'Unknown keyword: "{schema.keys()[0]}"')
-
-        shares = Shamir.split(threshold, num_shares, plaintext)
-
+        expression = to_cnf(  # Product of sums form
+            EncryptedShare.parse_schema(schema),
+            simplify=True,
+            force=True
+        )
         hashed_plaintext = SHA3_256.new(plaintext).hexdigest()
-        return [
-            EncryptedShare(
-                name=id,
-                ciphertext=share,
-                schema=schema,
-                hashed_plaintext=hashed_plaintext
-            )
-            for id, share in zip(lst, shares)
-        ]
+        if isinstance(expression, Symbol):  # Only one symbol at expression root
+            num_shares = threshold = 1
+            shares = Shamir.split(threshold, num_shares, plaintext, ssss=False)
+            result = [
+                EncryptedShare(
+                    name=expression.name,
+                    ciphertexts=shares,
+                    schema=schema,
+                    hashed_plaintext=hashed_plaintext
+                )
+            ]
+        elif isinstance(expression, Or):  # A secret shared in plaintext between multiple keyholders
+            num_shares = threshold = 1
+            shares = Shamir.split(threshold, num_shares, plaintext, ssss=False)
+            result = [
+                EncryptedShare(
+                    name=symbol.name,
+                    ciphertexts=shares,
+                    schema=schema,
+                    hashed_plaintext=hashed_plaintext
+                )
+                for symbol in expression.args
+            ]
+        elif isinstance(expression, And):
+            # A secret shared only in part to each key holder
+            num_shares = threshold = len(expression.args)
+            shares = Shamir.split(threshold, num_shares, plaintext, ssss=False)
+            symbols = expression.free_symbols
+            share_distribution = {symbol.name: set() for symbol in symbols}
+            for subexpression, share in zip(expression.args, shares):
+                for symbol in subexpression.free_symbols:
+                    share_distribution[symbol.name].add(share)
+            result = [
+                EncryptedShare(
+                    name=name,
+                    ciphertexts=shares,
+                    schema=schema,
+                    hashed_plaintext=hashed_plaintext
+                )
+                for name, shares in share_distribution.items()
+            ]
+        else:
+            # Should in theory never happen
+            raise RuntimeError('Unknown expression type!')
+
+        return result
 
     @staticmethod
     def decode(shares):
@@ -57,19 +93,20 @@ class EncryptedShare:
             raise ValueError('You must have at least one share!')
         if not EncryptedShare.satisfies(shares):
             raise ValueError('You do not have enough shares to decrypt this message.')
-        return Shamir.combine([share.ciphertext for share in shares])
+        return Shamir.combine(
+            list(set().union(*[share.ciphertexts for share in shares])),
+            ssss=False
+        )
 
     @staticmethod
-    def satisfies(shares):
+    def satisfies(shares: List):
         """Check whether a set of shares satisfies their schemas.
 
         :param shares: A collection of shares
-        :type shares: List[EncryptedShare]
-        :returns: A boolean indicating whether these shares are sufficient to decrypt the secret
-        :rtype: bool"""
+        :returns: A boolean indicating whether these shares are sufficient to decrypt the secret"""
         schema = shares[0].schema
         hashed_plaintext = shares[0].hashed_plaintext
-        ids = set()
+        names = set()
         for share in shares:
             if share.schema != schema:
                 raise ValueError(
@@ -84,26 +121,65 @@ class EncryptedShare:
                     f' shares were created with different plaintexts. All '
                     f'shares must have the same hash!'
                 )
-            ids.add(share.name)
+            names.add(share.name)
 
-        possible_ids, threshold, _ = EncryptedShare.parse_schema(schema)
-        return len(ids.intersection(possible_ids)) >= threshold
+        expression = EncryptedShare.parse_schema(schema)
+
+        return simplify_logic(
+            expression.subs({
+                Symbol(names): True
+                for names in names
+            })
+        ) is true
 
     @staticmethod
-    def parse_schema(schema):
+    def parse_schema(schema: Dict[str, List]):
         """Given a schema, find the parameters which it requires.
 
-        :param schema: An object recovered from json.load
-        :type schema: dict"""
+        :param schema: An object recovered from json.load"""
+        if not isinstance(schema, dict):
+            raise ValueError('Top level of schema must be a dictionary')
         if len(schema) != 1:
             raise ValueError('Schema must have exactly one top level expression')
-        if (possible_ids := schema.get('any')) or (possible_ids := schema.get('or')):
-            possible_ids = set(possible_ids)
-            threshold = 1
-            num_shares = len(possible_ids)
-        elif (possible_ids := schema.get('all')) or (possible_ids := schema.get('and')):
-            possible_ids = set(possible_ids)
-            threshold = num_shares = len(possible_ids)
+        (key, expressions), = schema.items()
+        if not isinstance(expressions, list):
+            raise ValueError('All dictionary values must be lists')
+        if len(expressions) == 0:
+            raise ValueError('Dictionary values may not be empty lists')
+
+        if key in ['or', 'any']:
+            combining_func = Or
+        elif key in ['all', 'and']:
+            combining_func = And
+        elif match := re.match(r'^at least (-?\d+)$', key):
+            minimum = int(match.group(1))
+            if minimum <= 0:
+                raise ValueError(f'In "{key}": number ({minimum}) must be a positive integer')
+
+            def at_least(*sym, minimum_present):
+                if minimum_present > len(sym):
+                    raise ValueError(
+                        f'Minimum number of expressions is'
+                        f' {minimum_present}, but there are only {len(sym)}'
+                        f' expressions present')
+                combinations = itertools.combinations(sym, minimum_present)
+                return Or(*[
+                    And(*combination)
+                    for combination in combinations
+                ])
+
+            combining_func = partial(at_least, minimum_present=minimum)
         else:
-            raise ValueError(f'Unknown keyword: "{schema.keys()[0]}"')
-        return possible_ids, threshold, num_shares
+            raise ValueError(f'Unknown keyword: "{key}"')
+
+        symbols = []
+        for expression in expressions:
+            if isinstance(expression, str):
+                symbols.append(Symbol(expression))
+            elif isinstance(expression, dict):
+                symbols.append(EncryptedShare.parse_schema(expression))
+            else:
+                raise ValueError(
+                    f'Unknown expression, expected str or dict: "{expression}"'
+                )
+        return combining_func(*symbols)
--- test.py
+++ test.py
@@ -11,12 +11,56 @@ def test_empty_decode():
 
 class TestInvalidEncode:
     def test_empty_encode(self):
+        secret = get_random_bytes(16)
         with pytest.raises(ValueError):
-            EncryptedShare.encode('abc', {})
+            EncryptedShare.encode(secret, {})
 
     def test_double_encode(self):
+        secret = get_random_bytes(16)
         with pytest.raises(ValueError):
-            EncryptedShare.encode('abc', {'any': ['a'], 'all': ['b']})
+            EncryptedShare.encode(secret, {'any': ['a'], 'all': ['b']})
+
+    def test_bad_key(self):
+        secret = get_random_bytes(16)
+        schema = {'does not exist': ['a', 'b', 'c', 'd']}
+        with pytest.raises(ValueError):
+            EncryptedShare.encode(secret, schema)
+
+    def test_value_not_list(self):
+        secret = get_random_bytes(16)
+        schema = {'any': 'haha I\'m not a list, I fooled you!'}
+        with pytest.raises(ValueError):
+            EncryptedShare.encode(secret, schema)
+
+    def test_value_empty_list(self):
+        secret = get_random_bytes(16)
+        schema = {'any': []}
+        with pytest.raises(ValueError):
+            EncryptedShare.encode(secret, schema)
+
+    def test_schema_not_dict(self):
+        secret = get_random_bytes(16)
+        schema = 'don\'t tell them I\'m not a dictionary'
+        with pytest.raises(ValueError):
+            EncryptedShare.encode(secret, schema)
+
+    def test_insufficient_at_least(self):
+        secret = get_random_bytes(16)
+        schema = {'at least 3': ['a']}
+        with pytest.raises(ValueError):
+            EncryptedShare.encode(secret, schema)
+
+    def test_invalid_minimum(self):
+        secret = get_random_bytes(16)
+        schema = {'at least -3': ['a', 'b', 'c']}
+        with pytest.raises(ValueError):
+            EncryptedShare.encode(secret, schema)
+
+    def test_invalid_value_entry(self):
+        secret = get_random_bytes(16)
+        schema = {'any': ['a', ['b', 'd', 'e'], 'c']}
+        with pytest.raises(ValueError):
+            EncryptedShare.encode(secret, schema)
 
 
 def test_single_custody():
@@ -52,3 +96,122 @@ def test_insufficient_copies():
             EncryptedShare.decode([share])
     with pytest.raises(ValueError):
         EncryptedShare.decode(shares[:3])
+
+
+def test_nested_any():
+    secret = get_random_bytes(16)
+    schema = {
+        'any': [
+            'a',
+            'b',
+            'c',
+            {'any': ['d', 'e', 'f']}
+        ]
+    }
+    shares = EncryptedShare.encode(secret, schema)
+    for share in shares:
+        result = EncryptedShare.decode([share])
+        assert result == secret
+
+
+def test_any_and_any():
+    secret = get_random_bytes(16)
+    schema = {
+        'all': [
+            {'any': ['a', 'b', 'c']},
+            {'any': ['d', 'e', 'f']}
+        ]
+    }
+    shares = EncryptedShare.encode(secret, schema)
+    shares_by_name = {share.name: share for share in shares}
+    with pytest.raises(ValueError):
+        EncryptedShare.decode([shares_by_name['a']])
+    with pytest.raises(ValueError):
+        EncryptedShare.decode([shares_by_name['a'], shares_by_name['b']])
+    with pytest.raises(ValueError):
+        EncryptedShare.decode([shares_by_name['a'], shares_by_name['b'], shares_by_name['c']])
+
+    result = EncryptedShare.decode([shares_by_name['b'], shares_by_name['e']])
+    assert result == secret
+
+
+def test_at_least():
+    secret = get_random_bytes(16)
+    schema = {
+        'at least 2': [
+            {'any': ['a', 'b', 'c']},
+            {'any': ['d', 'e', 'f']},
+            {'any': ['g', 'h', 'i']}
+        ]
+    }
+    shares = EncryptedShare.encode(secret, schema)
+    shares_by_name = {share.name: share for share in shares}
+    with pytest.raises(ValueError):
+        EncryptedShare.decode([shares_by_name['a']])
+    with pytest.raises(ValueError):
+        EncryptedShare.decode([shares_by_name['a'], shares_by_name['b']])
+    with pytest.raises(ValueError):
+        EncryptedShare.decode([shares_by_name['a'], shares_by_name['b'], shares_by_name['c']])
+
+    result = EncryptedShare.decode([shares_by_name['b'], shares_by_name['e']])
+    assert result == secret
+    result = EncryptedShare.decode([shares_by_name['f'], shares_by_name['g']])
+    assert result == secret
+    result = EncryptedShare.decode([shares_by_name['i'], shares_by_name['a']])
+    assert result == secret
+
+
+def test_mismatched_schema():
+    secret = get_random_bytes(16)
+    schema1 = {
+        'all': ['a', 'b', 'c']
+    }
+    schema2 = {
+        'at least 2': ['a', 'b', 'c']
+    }
+    shares1 = EncryptedShare.encode(secret, schema1)
+    shares2 = EncryptedShare.encode(secret, schema2)
+
+    with pytest.raises(ValueError):
+        EncryptedShare.decode([shares1[0], shares2[0]])
+
+
+def test_same_schema_different_secret():
+    secret1 = get_random_bytes(16)
+    secret2 = get_random_bytes(16)
+    schema = {
+        'at least 2': ['a', 'b', 'c']
+    }
+    shares1 = EncryptedShare.encode(secret1, schema)
+    shares2 = EncryptedShare.encode(secret2, schema)
+
+    with pytest.raises(ValueError):
+        EncryptedShare.decode([shares1[0], shares2[0]])
+
+
+def test_complicated_schema():
+    secret = get_random_bytes(16)
+    schema = {
+        'any': [
+            {'at least 2': ['a', 'b', 'c']},
+            {'all': [
+                'a',
+                {'at least 5': ['d', 'e', 'f', 'g', 'h', 'i', 'j']}
+            ]}
+        ]
+    }
+    shares = EncryptedShare.encode(secret, schema)
+    shares_by_name = {share.name: share for share in shares}
+
+    result = EncryptedShare.decode([shares_by_name['a'], shares_by_name['c']])
+    assert result == secret
+
+    result = EncryptedShare.decode([
+        shares_by_name['a'],
+        shares_by_name['f'],
+        shares_by_name['g'],
+        shares_by_name['e'],
+        shares_by_name['j'],
+        shares_by_name['i']
+    ])
+    assert result == secret
